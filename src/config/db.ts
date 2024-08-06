55cd9071465e3ad869ea899246bd764c089ac902
Franklin-Gomez <franklin_043097@hotmail.com>
2024-08-06 11:26:17 -0500

    create tabla

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
 
 dotenv.config() // traernos las variables de entorno
 
 //const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
 
-export const db = new Sequelize(process.env.DATA_BASE_URL) 
+export const db = new Sequelize(process.env.DATA_BASE_URL , { 
+    models: [__dirname + '/../models/**/*.ts']
+}) 
 
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { db } from './config/db'
 export const connectDB = async () => { 
     try {
         await db.authenticate();
+        db.sync() // sincronizacion los modelos en automatico
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
