40d9e7aecda6e92f59418f8c84c4fa9b1f4e0de4
Franklin-Gomez <franklin_043097@hotmail.com>
2024-08-15 10:47:01 -0500

    add tests get selected register

--- src/handlers/__test__/products.test.ts
+++ src/handlers/__test__/products.test.ts
@@ -15,3 +15,33 @@ describe('test products endpoints' , () => {
     })
 
 })
+
+describe( 'get selected product ', () => { 
+    it('get the correct products /:id' , async () => { 
+
+        const products = await request(server).get('/api/products/14')
+    
+        expect(products.status).toBe(200)
+        expect(products.headers['content-type']).toMatch(/json/)
+        expect(products.body.data).toHaveProperty('id')
+        expect(products.body.data.price).toBeGreaterThan( 0 )
+       
+    })
+
+    it('get the wrong products /:id' , async () => { 
+        const products = await request(server).get('/api/products/2000')
+
+        expect(products.status).toBe(400)
+        expect(products.body).toHaveProperty('error')
+        
+    })
+
+    it('id is string and wrong', async () => { 
+        const products = await request(server).get('/api/products/hola')
+
+        expect(products.body.errors).toHaveLength(1)
+        expect(products.body.errores[0].msg).toMatch('seleccion no valida')
+        expect(products.body.errors[0]).toHaveProperty('msg')
+
+    }) 
+})
